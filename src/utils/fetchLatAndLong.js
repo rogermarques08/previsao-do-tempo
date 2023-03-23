375dbb51e733ff401b08b5d9c2a8226eff253d25
Roger Marques <marquesroger226@gmail.com>
2023-03-23 20:31:49 -0300

    [fix]: https

--- src/utils/fetchLatAndLong.js
+++ src/utils/fetchLatAndLong.js
@@ -1,7 +1,7 @@
 import key from '../key/apiKey';
 
 const fetchLatAndLong = async (city) => {
-  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${key}`;
+  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${key}`;
   const response = await fetch(url);
   const data = await response.json();
 
